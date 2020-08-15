8d7d8e8da19faf7b1ad55b32741ded3a774cd645
Vadim Kantorov <vadimkantorov@gmail.com>
2020-08-15 23:33:31 +0200

    Update torchwave.py

--- torchwave.py
+++ torchwave.py
@@ -4,11 +4,10 @@ import torch
 
 def read(filename):
     with wave.open(filename, 'r') as w:
-        fs = w.getframerate()
         tensor, storage = (torch.ShortTensor, torch.ShortStorage) if w.getsampwidth() == 2 else (torch.CharTensor, torch.CharStorage)
         frames = w.readframes(w.getnframes())
         data = tensor(storage.from_buffer(frames, byte_order = 'native')).reshape(-1, w.getnchannels())
-        return fs, data.squeeze(1)
+        return w.getframerate(), data.squeeze(1)
 
 def write(filename, rate, data):
     assert data.is_contiguous() and not data.is_floating_point()
